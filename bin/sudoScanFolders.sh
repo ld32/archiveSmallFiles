405818b0cc981caa9a4c092981e3c27127612cef
ld32 <ld32@users.noreply.github.com>
2026-04-17 15:03:19 -0400

    de

--- bin/sudoScanFolders.sh
+++ bin/sudoScanFolders.sh
@@ -136,8 +136,9 @@ if [[ $count -gt 10000 ]]; then
   done
 fi
 
+echo -e "Scan results (should match with the numbers in starfish):\nTotal number of folders:\n$count\nAnd total number of files:"  | tee -a summary
+
 count=$(sudo find "$sDir" -maxdepth 1 -type f -o -type l | wc -l)
 printf "%s\t%s\n" "$count" "$sDir" >> $folders.withCount
-echo -e "Scan result:\nTotal number of files (should be the same as the number of files in starfish):" | tee -a $logDir/summary
-awk '{sum += $1} END {print sum}' $folders.withCount  | tee -a $logDir/summary
 
+awk '{sum += $1} END {print sum}' $folders.withCount  | tee -a summary
